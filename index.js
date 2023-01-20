29e4ea0381cdb4aae1e125926fe6489b94b8e661
MakrFrost <frostmark@ukr.net>
2023-01-20 14:59:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,16 +12,16 @@
 
 // console.log(Arr);
 
-const [...itemsArr] = document.querySelectorAll(".item-grid-card");
+// const [...itemsArr] = document.querySelectorAll(".item-grid-card");
 
-let Arr = [];
-itemsArr.map((item) => {
-  const mp3 = item.childNodes[1].getAttribute(
-    "data-audio-player-preview-url-value"
-  );
-  const title = item.childNodes[3].childNodes[1].innerHTML;
-  const author = item.childNodes[3].childNodes[3].innerHTML;
-  Arr.push({ author: author, mp3: mp3, title: title });
-});
+// let Arr = [];
+// itemsArr.map((item) => {
+//   const mp3 = item.childNodes[1].getAttribute(
+//     "data-audio-player-preview-url-value"
+//   );
+//   const title = item.childNodes[3].childNodes[1].innerHTML;
+//   const author = item.childNodes[3].childNodes[3].innerHTML;
+//   Arr.push({ author: author, mp3: mp3, title: title });
+// });
 
-console.log(Arr);
+// console.log(Arr);
